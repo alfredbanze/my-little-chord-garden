c232e8e21bc7d12d4b2064fd88133e9c2c07b76a
Alfred Banze <alfredbanze@alfs-neu.local>
2026-07-01 07:33:23 +0400

    Sprint 4 - First MIDI chords

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,18 @@
 mod midi;
 
-use midi::{connect, note_off, note_on, MidiService};
+use midi::{MidiService};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .manage(MidiService::new())
-        .invoke_handler(tauri::generate_handler![connect, note_on, note_off])
+        .invoke_handler(tauri::generate_handler![
+            midi::connect,
+            midi::note_on,
+            midi::note_off,
+            midi::play_notes,
+            midi::stop_notes
+        ])
         .plugin(tauri_plugin_opener::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
